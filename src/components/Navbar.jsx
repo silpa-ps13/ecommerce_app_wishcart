8b14716dedc0c2db487b4678368d7207b9f3c76d
silpaPS <silpasilu88@gmail.com>
2023-07-04 13:12:23 +0530

    container modifief

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,25 @@ import styled from 'styled-components';
 
 const Container = styled.div`
 height:60px;
-background_color:black;
 `;
+const Wrapper = styled.div`
+padding:10px 20px;
+display:flex;
+justify-content:space-between;`
+const Left = styled.div`
+flex:1;
+`
+const Center = styled.div`
+flex:1;`
+const Right = styled.div`
+flex:1;`
 
 const Navbar = () => {
-  return <Container>NavBar</Container>;
+  return <Container>
+    <Wrapper>
+      <Left>left</Left>
+      <Center>Center</Center>
+      <Right>Right</Right>
+    </Wrapper></Container>;
 };
 export default Navbar;
