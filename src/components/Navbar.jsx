94cd23ed54789d36f1399bb9a518b5eed62496ba
silpaPS <silpasilu88@gmail.com>
2023-07-04 21:35:58 +0530

    completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import SearchIcon from '@mui/icons-material/Search';
+import { Badge } from '@mui/material';
+import { ShoppingCartOutlined } from '@mui/icons-material';
 
 const Container = styled.div`
 height:60px;
@@ -10,30 +13,56 @@ display:flex;
 justify-content:space-between;`
 const Left = styled.div`
 flex:1;
+display:flex;
+align-items:center;
+
 `
 const Language = styled.span`
 font-size:14px;
 cursor: pointer;
 `
-const SearchContainer=styled.input`
+const SearchContainer=styled.div`
+border: 0.5px solid lightgray;
+display:flex;
+align-items:center;
+margin-left:25px;
+padding:5px;
+`
+const Input= styled.input`
+border:none;
 `
 const Center = styled.div`
-flex:1;`
+flex:1;
+text-align:center`
+const Logo=styled.h1`
+font-weight:bold;
+`
 const Right = styled.div`
-flex:1;`
-
+flex:1;
+display:flex;
+align-items:center;
+justify-content:flex-end`
+const MenuItem =styled.div`
+font-size:14px;
+cursor:pointer;
+margin-left:25px;`
 const Navbar = () => {
   return <Container>
     <Wrapper>
       <Left>
         <Language>EN</Language>
         <SearchContainer>
-          input
-          icon
+         <Input/>
+          <SearchIcon style={{color:'gray',fontSize:16}}/>
         </SearchContainer>
       </Left>
-      <Center>Center</Center>
-      <Right>Right</Right>
+      <Center><Logo>W_ishCart.</Logo></Center>
+      <Right><MenuItem>REGISTER</MenuItem>
+      <MenuItem>SIGN IN</MenuItem>
+      <MenuItem><Badge badgeContent={4} color="primary">
+        <ShoppingCartOutlined/>
+        </Badge></MenuItem>
+      </Right>
     </Wrapper></Container>;
 };
 export default Navbar;
