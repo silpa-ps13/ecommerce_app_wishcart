079a99407258f4a5343fcdba05c87c3a545e2389
silpaPS <silpasilu88@gmail.com>
2023-07-06 00:02:20 +0530

    slider part completed

---
+++ src/data.js
@@ -0,0 +1,21 @@
+export const sliderItems=[
+    {
+        id:1,
+        img:'https://img.freepik.com/free-photo/portrait-young-stylish-girl-model-casual-summer-clothes-brown-hat-with-natural-makeup-glasses-isolated_158538-8562.jpg?w=740&t=st=1688579939~exp=1688580539~hmac=210ce11fb8e4851f5c545c8931dbac25dbd69d5a4eccc0d9380c57c730b67aec',
+        title:"SUMMER SALE",
+        desc:"Don't Compromise on STYLE! Get Flat 39% OFF for New Arrivals.",
+    },
+    {
+        id:2,
+        img:'https://img.freepik.com/free-photo/portrait-beautiful-happy-cute-brunette-woman-girl-casual-green-hipster-summer-clothes-with-no-makeup-isolated-white-sunglasses_158538-12285.jpg?w=360&t=st=1688580550~exp=1688581150~hmac=15ae02825038a6c795c63169bc02c3d292361f7fdc89226aa6f5842d60a4a6e9',
+        title:"AUTUMN COLLECTION",
+        desc:"Don't Compromise on STYLE! Get Flat 39% OFF for New Arrivals.",
+    },
+    {
+        id:3,
+        img:'https://img.freepik.com/free-photo/high-fashion-look-glamor-stylish-beautiful-young-woman-model-with-red-lips-summer-bright-colorful-hipster-cloth-black-beanie_158538-9425.jpg?w=360&t=st=1688580622~exp=1688581222~hmac=77dafd8de4cb29503a631392cc8cf4d231089c45fa6b67339162261009110186',
+        title:"WINTER COLLECTION",
+        desc:"Don't Compromise on STYLE! Get Flat 39% OFF for New Arrivals.",
+    },
+    
+]
